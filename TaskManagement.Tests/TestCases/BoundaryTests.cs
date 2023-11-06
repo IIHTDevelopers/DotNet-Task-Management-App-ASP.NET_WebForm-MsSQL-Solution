252826127f35be1fcccca533df4b0e8c85ce7c66
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-06 09:43:59 +0530

    Add files via upload

---
+++ TaskManagement.Tests/TestCases/BoundaryTests.cs
@@ -0,0 +1,198 @@
+﻿
+
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagementApp.DAL.Interfaces;
+using TaskManagementApp.DAL.Services;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TaskManagementApp.Tests.TestCases
+{
+    public class BoundaryTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly ITaskService _taskService;
+        public readonly Mock<ITaskRepository> taskservice = new Mock<ITaskRepository>();
+
+        private static string type = "Boundary";
+
+        public BoundaryTests(ITestOutputHelper output)
+        {
+            _taskService = new TaskService(taskservice.Object);
+            _output = output;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Add_Task_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                taskservice.Setup(repos => repos.Add()).Returns("");
+                var result = _taskService.Add();
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Delete_Task_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                taskservice.Setup(repos => repos.Delete()).Returns("");
+                var result = _taskService.Delete();
+
+                //Assertion
+                if (result!= null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_AddTask_ReturnsString()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                taskservice.Setup(repos => repos.Add()).Returns("");
+                var result = _taskService.Add();
+
+                //Assertion
+                if (result is string)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_DeleteTask_ReturnsString()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                taskservice.Setup(repos => repos.Delete()).Returns("");
+                var result = _taskService.Delete();
+
+                //Assertion
+                if (result is string)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+    }
+}
---
+++ TaskManagement.Tests/TestCases/ExceptionalTests.cs
@@ -0,0 +1,112 @@
+﻿
+
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementApp.DAL.Interfaces;
+using TaskManagementApp.DAL.Services;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TaskManagementApp.Tests.TestCases
+{
+    public class ExceptionalTests
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly ITaskService _taskService;
+        public readonly Mock<ITaskRepository> taskservice = new Mock<ITaskRepository>();
+
+        private static string type = "Exception";
+
+        public ExceptionalTests(ITestOutputHelper output)
+        {
+            _taskService = new TaskService(taskservice.Object);
+            _output = output;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_GetAll_Tasks_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                taskservice.Setup(repos => repos.GetAll()).Returns("");
+                var result = _taskService.GetAll();
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+        [Fact]
+        public async Task<bool> Testfor_Update_Tasks_NotNull()
+        {
+            //Arrange
+            var res = false;
+            string testName; string status;
+            testName = CallAPI.GetCurrentMethodName();
+            int id = 1;
+
+            //Action
+            try
+            {
+                taskservice.Setup(repos => repos.Update()).Returns("");
+                var result = _taskService.Update();
+                //Assertion
+                if (result != null)
+                {
+                    res = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Assert
+                status = Convert.ToString(res);
+                _output.WriteLine(testName + ":Failed");
+                await CallAPI.saveTestResult(testName, status, type);
+                return false;
+            }
+            status = Convert.ToString(res);
+            if (res == true)
+            {
+                _output.WriteLine(testName + ":Passed");
+            }
+            else
+            {
+                _output.WriteLine(testName + ":Failed");
+            }
+            await CallAPI.saveTestResult(testName, status, type);
+            return res;
+        }
+
+
+    }
+}
